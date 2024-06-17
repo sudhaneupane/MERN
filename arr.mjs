30191602d0c4fce89c2f4c6854bd462217a443c6
sudhaneupane <sudaneupane.11@gmail.com>
2024-06-17 12:31:51 +0545

    array

---
+++ arr.mjs
@@ -0,0 +1,4 @@
+import { f1 } from "./array.mjs";
+
+let fun=f1([2,4,8,7]);
+console.log(fun);
---
+++ array.mjs
@@ -0,0 +1,45 @@
+
+// Array=[]  
+// map = callback and return method
+/* Syntax: list.map((value,index)=>{
+    return body}) */
+
+
+/* 
+let list=["a","b","c"]
+// initially output =[empty]
+let data =list.map((value,index)=>{
+// return 5;
+// return value;
+// return index;
+// return `${value}${index}`
+console.log("Sudha");
+return index;
+
+})
+console.log(data); 
+
+ Use map method if both input and output array are same and bot input and output length are same   */
+
+//  let list =[1,2,3,4]
+//  let output=list.map((value,index)=>{
+//     return value*2
+//  })
+//  console.log(output);
+
+
+// let f1=(inputArr)=>{
+//     let output=inputArr.map((value,index)=>{
+//         return value*2
+//     })
+//     console.log(output);
+// }
+// export {f1}
+
+// split => string to array
+// join =>Array to string
+
+// console.log("Sudha".split(""));
+// console.log("sudha".split("u"));
+
+console.log(["s","u","d","h","a"].join(""));
