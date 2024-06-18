f8bfcfe57ff927fa97730554845f1052d8441052
sudhaneupane <sudaneupane.11@gmail.com>
2024-06-18 12:40:29 +0545

    array methods

--- arr.mjs
+++ arr.mjs
@@ -1,4 +1,38 @@
-import { f1 } from "./array.mjs";
+// import { f1 } from "./array.mjs";
 
-let fun=f1([2,4,8,7]);
-console.log(fun);
+// let fun=f1([2,4,8,7]);
+// console.log(fun);
+
+// foreach doesnot use return keyword
+// let list=[1,2,3]
+// let output=list.forEach((value,index)=>{
+//    console.log(index);;
+// })
+
+
+// includes only returns true or false
+// let arr = ["ram", "hari", "shyam", "roshan"];
+// let output=arr.includes("hari")
+// console.log(output);
+
+// push =>push value to last
+// pop  =>remove value from last
+// unshift  =>adds value to beginning
+// shift    =>remove value from beginning
+
+// let list =[5,1,2,3,4]
+// // list.push(5)
+// // list.pop()
+// // list.unshift(9)
+// list.shift()
+
+// console.log(list);
+
+
+
+// sort method sorts from ascending to descending
+// doesnt sort integers
+let list=['d','e','z','h','a','t',"A",'g']
+    // list=['Sudha',"sumi",'A']
+list.sort()
+console.log(list);
--- array.mjs
+++ array.mjs
@@ -1,9 +1,7 @@
-
-// Array=[]  
+// Array=[]
 // map = callback and return method
 /* Syntax: list.map((value,index)=>{
     return body}) */
-
 
 /* 
 let list=["a","b","c"]
@@ -27,7 +25,6 @@ console.log(data);
 //  })
 //  console.log(output);
 
-
 // let f1=(inputArr)=>{
 //     let output=inputArr.map((value,index)=>{
 //         return value*2
@@ -42,4 +39,60 @@ console.log(data);
 // console.log("Sudha".split(""));
 // console.log("sudha".split("u"));
 
-console.log(["s","u","d","h","a"].join(""));
+// console.log(["s","u","d","h","a"].join(""));
+
+// filter return only true or false
+// let list=['a','b','c']
+// let list1=list.filter((value,index)=>{
+//     return true ;
+// })
+// console.log(list1);
+
+// let list =['a','b','c','d']
+// let list1=list.filter((value,index)=>{
+//     if(value==='d'){
+//         return true
+//     }
+//     else
+//         return false
+// })
+// console.log(list1);
+
+// let l=[1,2,3,6]
+// let li=l.filter((value,index)=>{
+//     // if(value===2 || value ===6){
+//     //     return true
+//     // }
+
+//     if (value%2===0) {
+//         return true
+//     }
+//     else
+//         return false
+// })
+// console.log(li);
+
+//find  returns the value that match the first value
+// let li=[1,2,3,4]
+// let le=li.find((value,index)=>{
+//     return true
+// })
+
+// console.log(le);
+
+// let input=[1,2,"sudha",true,false,"hello",'']
+// let output=input.filter(Boolean)
+// console.log(output);
+
+// ['ram','hari','shyam','roshan'] =4>
+
+// let arr = ["ram", "hari", "shyam", "roshan"];
+// let output = arr.filter((value, index) => {
+//   if (value.length > 4) {
+//     return true;
+//   } else return false;
+// });
+
+// console.log(output);
+
+
